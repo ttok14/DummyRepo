4670757df20ea346851fccf30e4967ce133de771
LeeYunSeon <ttok14@naver.com>
2021-01-17 23:13:37 +0900

    FirstCommit

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+class MyEditorScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "MyTestProject";
+    static string TARGET_DIR = @"C:\Users\Jayce\Desktop";
+
+    [MenuItem("Custom/CI/Build Mac OS X")]
+    static void PerformMacOSXBuild()
+    {
+        string target_dir = APP_NAME + ".app";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+    }
+
+    [MenuItem("Custom/CI/Android")]
+    static void PerformAndroidBuild()
+    {
+        string target_dir = APP_NAME + ".apk";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        BuildReport report = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}

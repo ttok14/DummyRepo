ab86ea5daedecbc060e1024641c6c63d458d4274
LeeYunSeon <ttok14@naver.com>
2021-01-24 23:36:04 +0900

    cli test code added

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 using UnityEditor.Build.Reporting;
+using System.IO;
 
 class MyEditorScript
 {
@@ -22,6 +25,59 @@ class MyEditorScript
     {
         string target_dir = APP_NAME + ".apk";
         GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
+
+
+        /////////-------------------------//
+
+        var a = System.Environment.GetCommandLineArgs();
+
+        if (a != null && a.Length > 1)
+        {
+            var sr = File.CreateText("File" + DateTime.Now.Second);
+
+            try
+            {
+                string prefix = "-CIBUILD:";
+                string all = a.Where(t_t => t_t.Contains(prefix)).Single();
+                string extracted = all.Replace(prefix, "");
+
+                Dictionary<string, string> dataBuffer = new Dictionary<string, string>();
+
+                string[] vars = extracted.Split(';');
+
+                foreach (var t in vars)
+                {
+                    string[] pair = t.Split('=');
+
+                    if (pair.Length != 2)
+                    {
+                        //    Console.WriteLine("separaator Must be =");
+                        continue;
+                    }
+
+                    sr.WriteLine($"VariableName : {pair[0]} , VariableValue : {pair[1]}");
+
+                    //     Console.WriteLine("Variable : " + pair[0] + " Is " + pair[1]);
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Dispose();
+                    sr = null;
+                }
+            }
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
     }
 
     private static string[] FindEnabledEditorScenes()
